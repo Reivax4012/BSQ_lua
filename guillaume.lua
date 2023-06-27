183059ec340c85a02bc04642daa6e3ed47e4a716
Reivax4012 <xavier.huber@epitech.eu>
2023-06-27 16:00:41 +0200

    Add: find biggest square but not location

--- guillaume.lua
+++ guillaume.lua
@@ -32,3 +32,45 @@ file:close()
 --     io.write(value .. " ")
 -- end
 -- io.write("\n")
+
+local rows = #array
+local cols = #array[1]
+
+local temp = {}
+for i = 1, rows do
+    temp[i] = {}
+    for j = 1, cols do
+        temp[i][j] = 0
+    end
+end
+
+local maxSquareSize = 0
+local maxSquareRow = 0
+local maxSquareCol = 0
+
+for i = 1, rows do
+    for j = 1, cols do
+        if i == 1 or j == 1 then
+            temp[i][j] = array[i][j]
+        elseif array[i][j] == 1 then
+            temp[i][j] = math.min(temp[i-1][j], temp[i][j-1], temp[i-1][j-1]) + 1
+        end
+
+        if temp[i][j] > maxSquareSize then
+            maxSquareSize = temp[i][j]
+            maxSquareRow = i
+            maxSquareCol = j
+        end
+    end
+end
+
+if maxSquareSize > 0 then
+    print("biggest square:")
+    for i = maxSquareRow - maxSquareSize + 1, maxSquareRow do
+        for j = maxSquareCol - maxSquareSize + 1, maxSquareCol do
+            io.write(array[i][j] .. " ")
+        end
+        io.write("\n")
+    end
+end
+
